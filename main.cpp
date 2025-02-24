ee25d7a194ad69b01f90d61620f024fb05963000
Ahsen Ali <ahsenali@192.168.1.13>
2025-02-24 17:32:19 +0500

    Your commit message here

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main () {
+  
+
+  int x = 0;
+
+  if(x == 5) 
+  {
+    std::cout << "x is 5\n";
+  }
+  else 
+  {
+    std::cout << "x is npt 5\n"; 
+  }
+  return 0;
+}
